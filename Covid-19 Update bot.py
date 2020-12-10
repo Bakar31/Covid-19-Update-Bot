48a5f73233c66ad7dee16a17cb71616c94fe48de
Abu Bakar <abubakar1808031@gmail.com>
2020-12-10 21:17:23 +0600

    Add files via upload

---
+++ Covid-19 Update bot.py
@@ -0,0 +1,22 @@
+import requests
+import json
+from win10toast import ToastNotifier
+import time
+
+def covid_update():
+    response = requests.get('https://coronavirus-19-api.herokuapp.com/countries')
+    data = response.json()
+    confirmed_cases = data[26]['cases']
+    deaths = data[26]['deaths']
+    today_cases = data[26]['todayCases']
+    today_deaths = data[26]['todayDeaths']
+    recovered = data[26]['recovered']
+    text = f'Confirmed_cases: {confirmed_cases}\nDeaths: {deaths}\nToday_cases: {today_cases}\nToday_deaths: {today_deaths}\nRecovered: {recovered}'
+
+    while True:
+        
+        toast = ToastNotifier()
+        toast.show_toast('Covid-19 Updates Bangladesh', text, duration = 15)
+        time.sleep(8)
+        
+covid_update()
