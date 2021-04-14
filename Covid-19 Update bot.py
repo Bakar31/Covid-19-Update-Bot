359e13be1418a19692afcba0671e78bde61931a8
Abu Bakar <abubakar1808031@gmail.com>
2021-04-14 15:07:31 +0600

    Update Covid-19 Update bot.py

--- Covid-19 Update bot.py
+++ Covid-19 Update bot.py
@@ -1,9 +1,14 @@
+#Importing required libraries
 import requests
 import json
 from win10toast import ToastNotifier
 import time
-
-def covid_update():
+ 
+def covid_update(): 
+    '''
+    this function takes no parameter, will get a response from the given api and access the values in Bangladesh row from the response data.
+    Then return the text with proper values and display itt as notification.
+    '''
     response = requests.get('https://coronavirus-19-api.herokuapp.com/countries')
     data = response.json()
     confirmed_cases = data[26]['cases']
@@ -13,8 +18,7 @@ def covid_update():
     recovered = data[26]['recovered']
     text = f'Confirmed_cases: {confirmed_cases}\nDeaths: {deaths}\nToday_cases: {today_cases}\nToday_deaths: {today_deaths}\nRecovered: {recovered}'
 
-    while True:
-        
+    while True: 
         toast = ToastNotifier()
         toast.show_toast('Covid-19 Updates Bangladesh', text, duration = 15)
         time.sleep(10)
